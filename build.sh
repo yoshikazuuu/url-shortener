86ea8f111e97745a83f3eb094ecb7c132f5df724
yoshikazuuu <febrianojerry@gmail.com>
2024-10-01 00:43:42 +0700

    fix: dockerfile

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t yoshikazuuu/url-shortener-backend:latest ./backend
-docker build -t yoshikazuuu/url-shortener-frontend:latest ./frontend
+docker build --platform=linux/amd64 --push -t yoshikazuuu/url-shortener-backend:latest ./backend
+docker build --platform=linux/amd64 --push -t yoshikazuuu/url-shortener-frontend:latest ./frontend
 
 docker push yoshikazuuu/url-shortener-backend:latest
 docker push yoshikazuuu/url-shortener-frontend:latest
